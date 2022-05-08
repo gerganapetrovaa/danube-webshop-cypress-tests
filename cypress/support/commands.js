107af4adf8c980876b26955aef1925f94f918cf4
gerganapp <gergana@atida.com>
2022-05-08 22:43:41 +0300

    add faker

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,18 @@
+import { faker } from '@faker-js/faker';
+
+
 Cypress.Commands.add('SignUpToApp', () => {
+    const randomFirstName = faker.name.firstName()
+    const randomLastName = faker.name.lastName()
+    const randomEmail = faker.internet.email()
+    const randomPassword = faker.internet.password()
+
     cy.visit('/')
     cy.get('#signup').click()
-    cy.get('#s-name').type('Gergana')
-    cy.get('#s-surname').type('Petrova')
-    cy.get('#s-email').type('gerganapp01@gmail.com')
-    cy.get('#s-password2').type('Test123!')
+    cy.get('#s-name').type(randomFirstName)
+    cy.get('#s-surname').type(randomLastName)
+    cy.get('#s-email').type(randomEmail)
+    cy.get('#s-password2').type(randomPassword)
     cy.get('#myself').check()
     cy.get('#privacy-policy').check()
     cy.get('#register-btn').click()
